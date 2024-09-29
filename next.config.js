4d1787f9bcebf5ca1829b4f912d2c5d6f506d63a
mohwildan <mobafreewtf@gmail.com>
2024-09-29 18:32:03 +0700

    update packages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
   },
+  webpack: (config, { isServer }) => {
+    // Add CSS rule for both client and server-side rendering
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      })
+    }
+
+    return config
+  },
 }
 
 module.exports = withTM(nextConfig)
